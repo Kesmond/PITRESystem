a80c89db6d11a262d68d8d37725a457075701af8
Kenneth Esmond <kennethesmond@gmail.com>
2025-05-17 19:17:21 +0800

    UI handles TFN and -1 properly

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 import xmlrpc.client
 
 def biweeklyIncomeCalculator():
-    print("Enter up to 26 biweekly income and tax withheld (e.g. 'income tax')")
+    print("\nEnter up to 26 biweekly income and tax withheld (e.g. '1000 100')")
     tax_pairs = []
     sum_income = 0
     sum_tax = 0
@@ -16,6 +16,13 @@ def biweeklyIncomeCalculator():
 
         try:
             income, tax = map(float, weekly_pair.split())
+            if income < 0 or tax < 0:
+                print("Values cannot be negative. Please try again.")
+                continue
+            elif tax > income:
+                print("Please enter the right amount.")
+                continue
+
             sum_income = sum_income + income
             sum_tax = sum_tax + tax
             tax_pairs.append((income,tax))
@@ -23,44 +30,81 @@ def biweeklyIncomeCalculator():
         except ValueError:
             print("Invalid input. Please enter two numbers, separated by space.")
 
-    return sum_income, sum_tax
+    return tax_pairs
+    #return sum_income, sum_tax
 
 
 def main():
     #Authentication
     #ID 6 digit and password
-    #uri = input("Authentication/Enter the server URI: ")
     proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+    #username = input("Username: ")
+    #password = input("Password: ")
+    username = "admin"
+    password = "admin"
 
-    #Implementation
-    income_pairs = 0
-    print("Welcome User!")
-    print("Please enter your 8 digit TFN number! (-1 if you don't have)")
-    tfn = int(input("Enter your Tax File Number (TFN): "))
-    print(tfn)
-    if tfn == -1:
-        print("Authentication Failed!\n")
-        income_pairs = biweeklyIncomeCalculator()
-    #print(income_pairs)
-    
-    have_PHIC = input("\nDo you have a Private Health Insurance Cover? (y/n): ").lower() == 'y'
+    if username == 'admin' and password == 'admin':
+        #Implementation
+        income_pairs = []
+        has_TFN = False
+        print("\nWelcome User!")
+        print("Please enter your 8 digit TFN number! (-1 if you don't have)")
+        
+        while True:
+            tfn = input("Enter your Tax File Number (TFN): ")
+            if tfn == "-1":
+                tfn = -1
+                has_TFN = False
+                income_pairs = biweeklyIncomeCalculator()
+                break
 
-    data = [income_pairs, have_PHIC]
-    try:
-        result = proxy.get_data(data)
+            if tfn.isdigit() and len(tfn) == 8:
+                has_TFN = True
+                break
+            else:
+                print("Invalid TFN format. It must be 8 digits or -1 if not available")
+            
+        while True:
+            try:
+                have_PHIC = input("\nDo you have a Private Health Insurance Cover? (y/n): ").lower()
+                if have_PHIC == 'y':
+                    have_PHIC = True
+                    break
+                elif have_PHIC == 'n':
+                    have_PHIC = False
+                    break
+                else:
+                    raise ValueError("Invalid input. Please enter 'y' or 'n'.")
+                    break
+            except ValueError as e:
+                print(f"Error: {e}")
 
-        if type(result) is str:
-            print()
-        else:
-            print("")
-            #print(ID)
-            print("NO TFN")
-            print(f"Annual Taxable Income: ", result[0])
-            print(f"Total Tax Witheld: ", result[1])
-            print(f"Total net-income: ", result[2])
-            print(f"Estimated tax refund amount: ", result[3])
-    except Exception as e:
-        print("Error {e}")
+        if has_TFN:
+            income_pairs.append((0,0))
+        
+        data = [income_pairs, have_PHIC]
+        #print(data)
+        try:
+            result = proxy.get_data(data)
+
+            if type(result) is str:
+                print("No Return")
+            else:
+                print("")
+                #print(ID)
+                print("NO TFN")
+                print(f"Annual Taxable Income:", result[0])
+                print(f"Total Tax Witheld:", result[1])
+                print(f"Total net-income:", result[2])
+                if result[3] >= 0:
+                    print(f"Estimated tax refund of:", result[3])
+                elif result[3] < 0:
+                    print(f"Estimated tax amount of", result[3]*-1, "owing to the ATO")
+                print(f"Total tax:", result[4])
+        except Exception as e:
+            print(f"Error {e}")
+    else:
+        print("Authentication failed!")
 
 if __name__ == "__main__":
     main()
