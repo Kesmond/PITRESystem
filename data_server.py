52fac1bbe66d95c6faae38353db279687abaf072
Kenneth Esmond <kennethesmond@gmail.com>
2025-05-17 19:18:35 +0800

    Im hungry, database isnt done

--- data_server.py
+++ data_server.py
@@ -1,1 +1,27 @@
 #data_server.py
+import sqlite3
+
+con = sqlite3.connect("PITD.db")
+cur = con.cursor()
+
+cur.execute("""
+CREATE TABLE tax_payers (
+            id INTEGER PRIMARY KEY,
+            tfn INTEGER UNIQUE NOT NULL,
+            record_id INTEGER
+)
+            """)
+
+cur.execute("""
+CREATE TABLE payroll_records (
+            record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            taxpayer_TFN INTEGER NOT NULL,
+            pay_period INTEGER,
+            payday TEXT,
+            gross_wage REAL,
+            tax_levied REAL,
+            net_wage REAL,
+            FOREIGN KEY (taxpayer_TFN) REFERENCES tax_payers (tfn)
+)
+            """)
+
